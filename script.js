765b340dd933d5a8ee7b9454068933c289d4c483
Abhiraj Singh <sabhiraj851@gmail.com>
2025-06-22 13:24:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-function addTask() {
-  const taskInput = document.getElementById('taskInput');
-  const taskText = taskInput.value.trim();
-  if (taskText === '') return;
+const input = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
-  const li = document.createElement('li');
-  li.textContent = taskText;
+let tasks = JSON.parse(localStorage.getItem("todoList")) || [];
+renderTasks();
 
-  const delBtn = document.createElement('button');
-  delBtn.innerHTML = '<img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" width="20" alt="Delete" />';
-  delBtn.style.marginLeft = '10px';
-  delBtn.style.cursor = 'pointer';
-  delBtn.style.border = 'none';
-  delBtn.style.background = 'none';
+addBtn.addEventListener("click", () => {
+  const taskText = input.value.trim();
+  if(taskText === "") return;
 
-  delBtn.onclick = () => li.remove();
+  tasks.push(taskText);
+  localStorage.setItem("todoList", JSON.stringify(tasks));
 
-  li.appendChild(delBtn);
-  document.getElementById('taskList').appendChild(li);
-  taskInput.value = '';
+  input.value = "";
+  renderTasks();
+});
+
+function renderTasks() {
+  taskList.innerHTML = ""; 
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.innerText = task;
+
+    const delBtn = document.createElement("button");
+   delBtn.innerHTML = `<img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" width="20" alt="delete" />`;
+
+    delBtn.style.marginLeft = "10px";
+
+    delBtn.addEventListener("click", () => {
+
+      tasks.splice(index, 1);
+      localStorage.setItem("todoList", JSON.stringify(tasks));
+      renderTasks(); 
+    });
+
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+  });
 }
